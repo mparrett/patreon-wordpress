19cf537bb8f52aaca2735ca9ef0526feee43d08a
Matt Parrett <matt@plainvanillagames.com>
2016-10-25 16:21:42 -0700

    Add debug shortcode

--- classes/patreon_frontend.php
+++ classes/patreon_frontend.php
@@ -25,8 +25,22 @@ class Patreon_Frontend
         add_filter('the_content', array($this, 'protectContentFromUsers'));
 
         add_shortcode('patreon_login_button', array($this, 'getPatreonButton'));
+        add_shortcode('debug_patreon_user', array($this, 'debugPatreonUser'));
     }
 
+    public function debugPatreonUser()
+    {
+        $ret = 'DEBUG: ';
+        $ret .= get_user_meta('patreon_refresh_token'); $ret .= '<br/>';
+        $ret .= get_user_meta('patreon_access_token'); $ret .= '<br/>';
+        $ret .= get_user_meta('patreon_user'); $ret .= '<br/>';
+        $ret .= get_user_meta('patreon_created'); $ret .= '<br/>';
+        $ret .= get_user_meta('user_firstname'); $ret .= '<br/>';
+        $ret .= get_user_meta('user_lastname'); $ret .= '<br/>';
+        $ret .= sprintf("$%.2f", Patreon_Wordpress::getUserPatronage() / 100); $ret .= "<br/>";        
+        return $ret;
+    }
+    
     public function showPatreonButton()
     {
         echo $this->getPatreonButton();
